e4700bb0589ed5b2d49956e6e4caf173fb9317be
Andrew Mambyk <witflash@gmail.com>
2018-06-05 12:18:16 +0300

    add lib copy for css and js

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -3,6 +3,8 @@ const config = require('../config');
 
 gulp.task('copy', () => {
   gulp.src(`${config.src.css}/*.css`).pipe(gulp.dest(config.dest.css));
+  gulp.src(`${config.src.css}/lib/*.css`).pipe(gulp.dest(`${config.dest.css}/lib/`));
   gulp.src(`${config.src.js}/*.js`).pipe(gulp.dest(config.dest.js));
+  gulp.src(`${config.src.js}/lib/*.js`).pipe(gulp.dest(`${config.dest.js}/lib/`));
   gulp.src(`${config.src.fonts}/**/*`).pipe(gulp.dest(config.dest.fonts));
 });
